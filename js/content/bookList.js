bece8b7636b805df9e4b407148ddf961847a18f2
Egor1496 <Egor1496Work@mail.ru>
2023-11-30 12:34:14 +0300

    0.7.3 dragonList

---
+++ js/content/bookList.js
@@ -0,0 +1,15 @@
+// const bookList = [{}];
+
+// const bookUL = $(""),
+// 	bookULDescr = $("");
+
+// let htmlBookUL = "",
+// 	htmlBookULDescr = "";
+
+// bookList.forEach((book, i) => {
+// 	htmlBookUL += ``;
+// 	htmlBookULDescr += ``;
+// });
+
+// bookUL.html(htmlBookUL);
+// bookULDescr.html(htmlBookULDescr);
